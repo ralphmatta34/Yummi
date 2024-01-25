5a5bd32794361f04d12c37e51815cc61ecb062b9
Ralph Matta <matta.r@etoncollege.org.uk>
2024-01-25 12:45:57 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Matta, Ralph (PEPW) on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
