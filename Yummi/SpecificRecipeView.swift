2e4b74146a9b43f68352c25b43c879c21f6cf527
Ralph Matta <matta.r@etoncollege.org.uk>
2024-02-27 11:14:03 +0000

    Made further progress with displaying recipes, including adding stars depending on the rating.

---
+++ Yummi/SpecificRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  SpecificRecipeView.swift
+//  Yummi
+//
+//  Created by Matta, Ralph (PEPW) on 27/02/2024.
+//
+
+import SwiftUI
+
+struct SpecificRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpecificRecipeView()
+}
